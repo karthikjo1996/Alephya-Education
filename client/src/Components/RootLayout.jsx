c298414673291d7ee77e711e2602f528ee4c30f8
karthikjo1996 <gkarthika950@gmail.com>
2025-01-03 09:05:16 +0530

    To add Topbar and root layout

--- client/src/Components/RootLayout.jsx
+++ client/src/Components/RootLayout.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import {Outlet} from 'react-router-dom';
+import TopBar from './TopBar';
+
 
 const RootLayout = () => {
   return (
     <div>
-      Root
+        {<TopBar/>}
+        <Outlet/>
     </div>
   )
 }
