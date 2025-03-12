1a1490cc41c6ac6480c76b81c3829d85eeedfde3
karthikjo1996 <gkarthika950@gmail.com>
2025-03-12 14:34:38 +0530

    create user model

---
+++ server/models/userModel.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema({
+   name :{type:String,required:true,unique:true},
+   email:{type:String,required:true,unique:true,unique:true},
+   password:{type:String,required:true},
+},{timestamps:true});
+
+const User = mongoose.model('user',userSchema);
+
+export default User;
